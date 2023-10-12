570a79c3e51992685d01a903b6b815b320e369b9
venkateshsure <venky007sure@gmail.com>
2023-10-12 08:36:54 +0000

    new changes

---
+++ src/components/Body/index.js
@@ -0,0 +1,65 @@
+// Write your code here
+
+import ConfigurationContext from '../../context/ConfigurationContext'
+import './index.css'
+
+const Body = () => (
+  <ConfigurationContext.Consumer>
+    {value => {
+      const {showContent, showRightNavbar, showLeftNavbar} = value
+      return (
+        <div className="con">
+          <div className="content-head-body">
+            <div className="left">
+              {showLeftNavbar && (
+                <>
+                  <h1>Left Navbar Menu</h1>
+                  <ul className="ul">
+                    <li>Item1</li>
+                    <li>Item2</li>
+                    <li>Item3</li>
+                    <li>Item4</li>
+                  </ul>
+                </>
+              )}
+            </div>
+            <div className="content">
+              {showContent && (
+                <>
+                  <h1>Content</h1>
+                  <p>
+                    Lorem ipsum developer door sit armet. I need to improve
+                    myself to get better in studies and also i need to bertter
+                    in communvarojeffjkjhe hcvkdsvjc
+                  </p>
+                </>
+              )}
+            </div>
+            <div className="right">
+              {showRightNavbar && (
+                <>
+                  <h1>Right Navbar</h1>
+                  <div className="right-fir-con">
+                    <p>Ad 1</p>
+                  </div>
+                  <div className="right-fir-con">
+                    <p>Ad 2</p>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      )
+    }}
+  </ConfigurationContext.Consumer>
+)
+
+export default Body
+
+/* showContent: true,
+  showLeftNavbar: true,
+  showRightNavbar: true,
+  onToggleShowContent: () => {},
+  onToggleShowLeftNavbar: () => {},
+  onToggleShowRightNavbar: () => {}, */
---
+++ src/components/ConfigurationController/index.js
@@ -0,0 +1,62 @@
+// Write your code here
+
+import ConfigurationContext from '../../context/ConfigurationContext'
+
+import './index.css'
+
+const ConfigurationController = () => {
+  const clickContent = onToggleShowContent => {
+    onToggleShowContent()
+  }
+
+  return (
+    <ConfigurationContext.Consumer>
+      {value => {
+        const {
+          showContent,
+          showLeftNavbar,
+          showRightNavbar,
+          onToggleShowContent,
+          onToggleShowRightNavbar,
+          onToggleShowLeftNavbar,
+        } = value
+        return (
+          <div className="config-con">
+            <div className="fir-config-con">
+              <h1 className="h1">Layout</h1>
+              <div className="checkbox-con">
+                <input
+                  type="checkbox"
+                  checked={showContent}
+                  onChange={onToggleShowContent}
+                  id="content"
+                />
+                <label htmlFor="content">Content</label>
+              </div>
+              <div className="checkbox-con">
+                <input
+                  onChange={onToggleShowLeftNavbar}
+                  checked={showLeftNavbar}
+                  type="checkbox"
+                  id="left navbar"
+                />
+                <label htmlFor="left navbar">Left Navbar</label>
+              </div>
+              <div className="checkbox-con">
+                <input
+                  onChange={onToggleShowRightNavbar}
+                  checked={showRightNavbar}
+                  type="checkbox"
+                  id="right navbar"
+                />
+                <label htmlFor="right navbar">Right Navbar</label>
+              </div>
+            </div>
+          </div>
+        )
+      }}
+    </ConfigurationContext.Consumer>
+  )
+}
+
+export default ConfigurationController
