c2d2b9d0a52eca5a85ad2e1ca0a65ae7712746db
venkateshsure <venky007sure@gmail.com>
2023-10-12 09:38:06 +0000

    new layout changes in app bulider

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ class App extends Component {
           onToggleShowContent: this.onToggleShowContent,
         }}
       >
-        return (
         <div>
           <ConfigurationController />
           <Layout />
